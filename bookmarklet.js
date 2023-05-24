4dc98d2d2e0735ae9a823bcae0f5d9d4ae6bc974
Viktar <1105714@gmail.com>
2023-05-24 18:28:02 +0400

    fix dist

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,9 +1,9 @@
 function copyToClipboard(text) {
-    const textarea = document.createElement('textarea');
+    const textarea = document.createElement("textarea");
     textarea.value = text;
     document.body.appendChild(textarea);
     textarea.select();
-    document.execCommand('copy');
+    document.execCommand("copy");
     document.body.removeChild(textarea);
 }
 
@@ -14,16 +14,16 @@ function fetchQuizletCards() {
         .then(response => response.text())
         .then(html => {
             const parser = new DOMParser();
-            const document = parser.parseFromString(html, 'text/html');
+            const document = parser.parseFromString(html, "text/html");
 
             const cards = [];
-            const cardElements = document.querySelectorAll('.SetPageTerm-content');
+            const cardElements = document.querySelectorAll(".SetPageTerm-content");
 
             cardElements.forEach(element => {
-                const termElement = element.querySelector('.SetPageTerm-wordText');
-                const definitionElement = element.querySelector('.SetPageTerm-definitionText');
-                const term = termElement ? termElement.textContent.trim() : '';
-                const definition = definitionElement ? definitionElement.textContent.trim() : '';
+                const termElement = element.querySelector(".SetPageTerm-wordText");
+                const definitionElement = element.querySelector(".SetPageTerm-definitionText");
+                const term = termElement ? termElement.textContent.trim() : "";
+                const definition = definitionElement ? definitionElement.textContent.trim() : "";
 
                 cards.push({ term, definition });
             });
@@ -31,21 +31,20 @@ function fetchQuizletCards() {
             return cards;
         })
         .then(cards => {
-            const output = cards.map(card => `${card.term}##${card.definition}`).join('####');
+            const output = cards.map(card => `${card.term}##${card.definition}`).join("####");
             return output;
         })
         .catch(error => {
-            console.error('Error fetching Quizlet cards:', error);
-            return '';
+            console.error("Error fetching Quizlet cards:", error);
+            return "";
         });
 }
 
-// Example usage
 fetchQuizletCards()
     .then(output => {
         console.log(output);
         copyToClipboard(output);
     })
     .catch(error => {
-        console.error('Error:', error);
+        console.error("Error:", error);
     });
